805bb6852fd74a6ecac6d3f71cebcbbe8115e64e
srividyas808-code <srividyas808@gmail.com>
2026-02-19 11:18:43 -0600

    Create print 1ton and n to 1 using for loop.c

---
+++ print 1ton and n to 1 using for loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int i,n;
+   printf("enter the value of n:");
+   scanf("%d",&n);
+   for(i=n;i>=1;i--)
+   {
+       printf(" %d",i);
+   }
+   for(i=1;i<=n;i++)
+   {
+       printf(" %d",i);
+   }
+   for(i=1;i<=10;i++)
+   {
+       printf(" %d",i);
+   }
+}
