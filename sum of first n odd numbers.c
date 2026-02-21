0942b364aa54614855e2b80e745aed702e7e19b0
srividyas808-code <srividyas808@gmail.com>
2026-02-21 11:57:57 -0600

    Create sum of first n odd numbers.c

---
+++ sum of first n odd numbers.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+int main()
+{
+    int i,n,sum=0;
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+        sum=sum+i;
+        }
+    }
+    printf("sum of first %d odd numbers %d",n,sum);
+}
